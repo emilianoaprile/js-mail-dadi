fb402dcc4d7a6d1b1405dc26a3cdf657b176fa28
EmilianoAprile <emilianoaprile77@icloud.com>
2023-10-09 14:55:51 +0200

    first commit: README email

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+// # Controlloemail 
+
+// ### chiediamo email utente
+// - inizializzare una variabile userInputEmail che prende l'input da un prompt
+// ### creaiamo array con emailList
+// - inizializzare un array con le email per le quali è consentito l'accesso
+// ### creaimo un ciclo for dell'array
+// - prendiamo index email che hanno la possibilità di accedere
+// ### stampiamo l'esito dell'accesso
+// SE userInputEmail = i
+// - stampa accesso consentito
+// ALTRIMENTI 
+// - stampa accesso negato
